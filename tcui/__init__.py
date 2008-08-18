208901ac183b43eee9fe79c4800b871cde2664a9
Timothy Baldock <tb@entropy.me.uk>
2008-08-18 00:42:33 +0000

    Lay foundations for UI module, tcui, will contain seperate modules for each user interface component to help split up the code, __init__.py contains a listing of the contents of the module
    git-svn-id: svn+ssh://entropy.me.uk/var/svn/timothy/trunk@17 741c516a-3ba6-4fa8-b112-5c51f26a88df

---
+++ tcui/__init__.py
@@ -0,0 +1,10 @@
+# coding: UTF-8
+#
+# TileCutter, User Interface Library
+#
+
+# Import all child controls
+import facingControl
+
+# Set value of * in import
+__all__ = ["facingControl"]
---
+++ tcui/facingControl.py
@@ -0,0 +1,4 @@
+# coding: UTF-8
+#
+# TileCutter, User Interface Module - facingControl
+#
