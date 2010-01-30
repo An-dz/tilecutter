8d6c260e2fdcdf1eacba1f865eae444d260198a9
Timothy Baldock <tb@entropy.me.uk>
2010-01-30 23:46:25 +0000

    Fix the "about" dialog so it isn't horribly broken on Unix, and hopefully more robust on windows
    git-svn-id: svn+ssh://entropy.me.uk/var/svn/timothy/tilecutter/trunk@371 741c516a-3ba6-4fa8-b112-5c51f26a88df

--- tcui/aboutDialog.py
+++ tcui/aboutDialog.py
@@ -20,40 +20,59 @@ class aboutDialog(wx.Dialog):
         """Intialise the dialog"""
         self.app = app
         self.version_number = version_number
-        size = (330,400)
-        wx.Dialog.__init__(self, parent, wx.ID_ANY, "", (-1,-1), size)
+        self.size = (400,1)
+        wx.Dialog.__init__(self, parent, wx.ID_ANY, "", (-1,-1), self.size)
 
-        # Overall panel sizer
-        self.s_panel = wx.BoxSizer(wx.VERTICAL)
+        # All items in panel sit inside individual horizontal sizers, which are contained
+        # within a vertical one
+        self.vbox = wx.BoxSizer(wx.VERTICAL)
+        self.hbox1 = wx.BoxSizer(wx.HORIZONTAL)
+        self.hbox2 = wx.BoxSizer(wx.HORIZONTAL)
+        self.hbox3 = wx.BoxSizer(wx.HORIZONTAL)
+        self.hbox4 = wx.BoxSizer(wx.HORIZONTAL)
+        self.hbox5 = wx.BoxSizer(wx.HORIZONTAL)
+        self.hbox6 = wx.BoxSizer(wx.HORIZONTAL)
+
+        # Big TileCutter icon
         self.icon = wx.StaticBitmap(self, wx.ID_ANY, imres.catalog["tc_icon2_128_plain"].getBitmap())
+        # Store default font to reset to later
         f = self.GetFont()
+        # Bigger font for title
         self.SetFont(wx.Font(18, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL))
-        self.title_text = wx.StaticText(self, wx.ID_ANY, "", (-1, -1), (-1, -1), wx.ALIGN_CENTER)
+        self.title_text = wx.StaticText(self, wx.ID_ANY, "", (-1, -1), (-1, -1), wx.ALIGN_CENTER_HORIZONTAL)
+        # Medium font for subtitle
         self.SetFont(wx.Font(12, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL))
-        self.subtitle_text = wx.StaticText(self, wx.ID_ANY, "", (-1, -1), (-1, -1), wx.ALIGN_CENTER)
+        self.subtitle_text = wx.StaticText(self, wx.ID_ANY, "", (-1, -1), (-1, -1), wx.ALIGN_CENTER_HORIZONTAL)
+        # Original font for version/copyright
         self.SetFont(f)
-        self.version_text = wx.StaticText(self, wx.ID_ANY, "", (-1, -1), (-1, -1), wx.ALIGN_CENTER)
-        self.copyright_text = wx.StaticText(self, wx.ID_ANY, "\n\n\n\n", (-1, -1), (-1, -1), wx.ALIGN_CENTER)
+        self.version_text = wx.StaticText(self, wx.ID_ANY, "", (-1, -1), (-1, -1), wx.ALIGN_CENTER_HORIZONTAL)
+        self.copyright_text = wx.StaticText(self, wx.ID_ANY, "", (-1, -1), (-1, -1), wx.ALIGN_CENTER_HORIZONTAL)
 
         # Add close button at the bottom
-        self.buttons = wx.BoxSizer(wx.HORIZONTAL)
         self.close_button = wx.Button(self, wx.ID_ANY, "", (-1,-1), (-1,-1), wx.ALIGN_RIGHT)
-        self.buttons.Add(self.close_button, 0 ,wx.ALIGN_RIGHT, 0)
 
-        self.s_panel.Add(self.icon,0,wx.EXPAND|wx.ALIGN_CENTER_HORIZONTAL|wx.TOP|wx.BOTTOM, 15)
-        self.s_panel.Add(self.title_text,0,wx.EXPAND|wx.ALIGN_CENTER_HORIZONTAL|wx.BOTTOM, 2)
-        self.s_panel.Add(self.subtitle_text,0,wx.EXPAND|wx.ALIGN_CENTER_HORIZONTAL|wx.BOTTOM, 4)
-        self.s_panel.Add(self.version_text,0,wx.EXPAND|wx.ALIGN_CENTER_HORIZONTAL|wx.BOTTOM, 4)
-        self.s_panel.Add(self.copyright_text,1,wx.EXPAND|wx.ALIGN_CENTER_HORIZONTAL|wx.BOTTOM, 10)
-        # These bits are non-mac only
-        self.s_panel.Add(wx.StaticLine(self, wx.ID_ANY, (-1,-1), (-1,1)),0,wx.EXPAND|wx.ALIGN_CENTER_HORIZONTAL, 0)
-        self.s_panel.Add(self.buttons,0,wx.ALIGN_RIGHT|wx.ALL, 3)
+        # Add all items to horizontal sizers, to get horizontal centering and expansion
+        self.hbox1.Add(self.icon,           0, wx.TOP|wx.BOTTOM, 15)
+        self.hbox2.Add(self.title_text,     0, wx.BOTTOM, 2)
+        self.hbox3.Add(self.subtitle_text,  0, wx.BOTTOM, 4)
+        self.hbox4.Add(self.version_text,   0, wx.BOTTOM, 10)
+        self.hbox5.Add(self.copyright_text, 0, wx.BOTTOM|wx.RIGHT|wx.LEFT, 10)
+        self.hbox6.Add(self.close_button,   1, wx.ALIGN_RIGHT, 0)
+
+        # Add horizontal sizers to the vertical one (along with a static line)
+        self.vbox.Add(self.hbox1, 0, wx.ALIGN_CENTER)
+        self.vbox.Add(self.hbox2, 0, wx.ALIGN_CENTER)
+        self.vbox.Add(self.hbox3, 0, wx.ALIGN_CENTER)
+        self.vbox.Add(self.hbox4, 0, wx.ALIGN_CENTER)
+        self.vbox.Add(self.hbox5, 1, wx.BOTTOM, 10)
+        self.vbox.Add(wx.StaticLine(self, wx.ID_ANY, (-1,-1), (-1,1)), 0, wx.EXPAND|wx.ALL, 3)
+        self.vbox.Add(self.hbox6, 0, wx.ALIGN_RIGHT|wx.ALL, 3)
 
         # Bind events
         self.close_button.Bind(wx.EVT_BUTTON, self.OnClose, self.close_button)
 
         # Layout sizers
-        self.SetSizer(self.s_panel)
+        self.SetSizer(self.vbox)
         self.SetAutoLayout(1)
 ##        self.Layout()
 
@@ -66,9 +85,14 @@ class aboutDialog(wx.Dialog):
         self.title_text.SetLabel(gt("TileCutter"))
         self.subtitle_text.SetLabel(gt("Simutrans Building Editor"))
         self.version_text.SetLabel(gt("Version %s") % self.version_number)
-        self.copyright_text.SetLabel("Copyright © 2008-2009 Timothy Baldock. All rights reserved.\n\n\nThis program makes use of the wxWidgets library, which is Copyright © 1992-2006 Julian Smart, Robert Roebling, Vadim Zeitlin and other members of the wxWidgets team\n\nTileCutter is written in Python")
+        self.copyright_text.SetLabel(u"\nCopyright \u00A9 2008-2009 Timothy Baldock. All rights reserved.\n\nThis program makes use of the wxWidgets library, which is Copyright \u00A9 1992-2006 Julian Smart, Robert Roebling, Vadim Zeitlin and other members of the wxWidgets team\n\nTileCutter is written in Python")
+        self.title_text.Wrap(self.size[0])
+        self.subtitle_text.Wrap(self.size[0])
+        self.version_text.Wrap(self.size[0])
+        self.copyright_text.Wrap(self.size[0])
 
         self.Layout()
+        self.Fit()
         self.Refresh()
 
     def update(self):
